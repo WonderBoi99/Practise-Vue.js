c8d4dc3085290152606e5b9ccf7f27725bbc5a38
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-06-27 14:10:19 -0600

    Update app.js

---
+++ gs-01-starting-project/app.js
@@ -0,0 +1,13 @@
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
+
+
+function addGoal(){
+    const goal = inputEl.value;
+    const newListItem = document.createElement('li');
+    newListItem.textContent = goal;
+    listEl.append(newListItem);
+}
+
+buttonEl.addEventListener('click', addGoal);
