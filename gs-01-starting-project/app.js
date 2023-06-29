23614af6d20da8c96f0502ba2bbefe27d4537920
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-06-28 20:43:00 -0600

    Update app.js

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,14 +1,15 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
 
-function addGoal(){
-    const goal = inputEl.value;
-    const newListItem = document.createElement('li');
-    newListItem.textContent = goal;
-    listEl.append(newListItem);
-    inputEl.value = '';
-}
+// function addGoal(){
+//     const goal = inputEl.value;
+//     const newListItem = document.createElement('li');
+//     newListItem.textContent = goal;
+//     listEl.append(newListItem);
+//     inputEl.value = '';
+// }
 
-buttonEl.addEventListener('click', addGoal);
+// buttonEl.addEventListener('click', addGoal);
+alert("BOBO");
