a0a091da8d402e7d16f53f52280dd211a94870ff
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-07-11 15:49:32 -0600

    Update App.vue

--- vue-first-app/src/App.vue
+++ vue-first-app/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TagWords></TagWords>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TagWords from './components/TagWords.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TagWords
   }
 }
 </script>
