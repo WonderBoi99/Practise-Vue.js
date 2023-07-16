2965808e3f4e3c05625af23745ec8fc3f9d6e19d
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-07-15 18:29:32 -0600

    Update App.vue

--- vue-cli-01-a-new-vue-project 2/src/App.vue
+++ vue-cli-01-a-new-vue-project 2/src/App.vue
@@ -1,4 +1,12 @@
 <template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <friend-contact/>
+            <friend-contact/>
+            <friend-contact/>
+        </ul>
+    </section>
     
 </template>
 
