ae6ceef5ddd1a1fbd9563e6b3eb9dc0f43bc36f4
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-07-15 18:12:56 -0600

    Create App.vue

---
+++ vue-cli-01-a-new-vue-project 2/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    
+</template>
+
+<script>
+
+export default ({
+   data(){
+    return{
+        friends:[
+            {
+                id: 1,
+                name:'bobby',
+                phone:345-345-3453,
+                email: 'bobby@email.com'
+            },
+            {
+                id: 2,
+                name:'sammy',
+                phone:375-665-4553,
+                email: 'sammy@email.com'
+            },
+        ]
+    };
+   }
+   
+})
+</script>
