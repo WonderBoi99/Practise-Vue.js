1f88af07c33e567925a0d14353c3d45abf6da0f2
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-08-31 12:41:33 -0600

    Update ActiveUser.vue

--- first-vue-project/src/components/ActiveUser.vue
+++ first-vue-project/src/components/ActiveUser.vue
@@ -1,15 +1,12 @@
 <template>
     <div>
         <h2>{{ firstname }}</h2>
-        <h3>{{ userage }}}</h3>  
+        <h3>{{ userage }}</h3>  
     </div>
 </template>
 
 <script>
 export default{
-    props:{
-        firstname, 
-        userage,
-    }
+    props:['firstname', 'userage']
 }
 </script>
