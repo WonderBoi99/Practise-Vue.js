2c2f5c7a9fe0824e54ef03f4ad3e4d3d88c43b94
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-09-27 09:51:06 -0600

    auth practise

--- first-vue-project/src/components/requests/RequestItem.vue
+++ first-vue-project/src/components/requests/RequestItem.vue
@@ -1,24 +1,22 @@
 <template>
-    <li>
-        <div>
-            <a href="emailLink">{{ email }}</a>
-        </div>
-        <p>{{ message }}</p>
-    </li>  
+  <li>
+    <div>
+      <a :href="emailLink">{{ email }}</a>
+    </div>
+    <p>{{ message }}</p>
+  </li>
 </template>
 
 <script>
 export default {
-    props: ['email', 'message'],
-    computed:{
-        emailLink(){
-            return 'mailto:' + this.email;
-        }
+  props: ['email', 'message'],
+  computed: {
+    emailLink() {
+      return 'mailto:' + this.email;
     }
+  }
 }
 </script>
-
-
 
 <style scoped>
 li {
